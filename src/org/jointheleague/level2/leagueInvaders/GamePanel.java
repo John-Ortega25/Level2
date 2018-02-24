fc6dc3208fb5880c4ad3158c0930b7f596f60e4b
John-Ortega25 <league@WorthyMini-3.local>
2018-02-24 12:22:28 -0800

    2-24-18
    Part 9 step 1

--- src/org/jointheleague/level2/leagueInvaders/GamePanel.java
+++ src/org/jointheleague/level2/leagueInvaders/GamePanel.java
@@ -14,7 +14,13 @@ import javax.swing.Timer;
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Timer timer;
 	Font titleFont;
-	Rocketship rocket= new Rocketship(250,700,50,50);
+	Font titleFont2;
+	Font titleFont3;
+	Font finalFont;
+	Font finalFont2;
+	Font finalFont3;
+
+	Rocketship rocket = new Rocketship(250, 700, 50, 50);
 	final int MENU_STATE = 0;
 	final int GAME_STATE = 1;
 	final int END_STATE = 2;
@@ -22,7 +28,13 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
 	public GamePanel() {
 		timer = new Timer(1000 / 60, this);
-		titleFont= new Font("Arial", Font.PLAIN, 48);
+		titleFont = new Font("Arial", Font.BOLD, 48);
+		titleFont2 = new Font("Arial", Font.BOLD, 24);
+		titleFont3 = new Font("Arial", Font.BOLD, 24);
+		finalFont = new Font("Arial", Font.PLAIN, 52);
+		finalFont2 = new Font("Arial", Font.BOLD, 24);
+		finalFont3 = new Font("Arial", Font.PLAIN, 24);
+
 	}
 
 	public void startGame() {
@@ -34,16 +46,14 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void paintComponent(Graphics graphics) {
 		object.draw(graphics);
-		if (currentState== MENU_STATE) {
+		if (currentState == MENU_STATE) {
 			drawMenuState(graphics);
-		}else if (currentState== GAME_STATE) {
+		} else if (currentState == GAME_STATE) {
 			drawGameState(graphics);
-		}else if (currentState== END_STATE) {
+		} else if (currentState == END_STATE) {
 			drawEndState(graphics);
 		}
-		
-		
-		
+
 	}
 
 	public void updateMenuState() {
@@ -52,7 +62,7 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
 	public void updateGameState() {
 		rocket.update();
-		
+
 	}
 
 	public void updateEndState() {
@@ -61,11 +71,18 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
 	public void drawMenuState(Graphics graphics) {
 		graphics.setColor(Color.BLUE);
-		graphics.fillRect(0,0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		graphics.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
 		graphics.setFont(titleFont);
 		graphics.setColor(Color.YELLOW);
-		graphics.drawString("LEAGUE INVADERS", 20, 80);
+		graphics.drawString("LEAGUE INVADERS", 20, 200);
+		graphics.setFont(titleFont2);
+		graphics.setColor(Color.YELLOW);
+		graphics.drawString("Press ENTER to start", 100, 350);
+		graphics.setFont(titleFont3);
+		graphics.setColor(Color.YELLOW);
+		graphics.drawString("Press SPACE for instructions", 50, 500);
 	}
+
 	public void drawGameState(Graphics graphics) {
 		graphics.setColor(Color.BLACK);
 		graphics.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
@@ -75,20 +92,27 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	public void drawEndState(Graphics graphics) {
 		graphics.setColor(Color.RED);
 		graphics.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-		graphics.setFont(titleFont);
+		graphics.setFont(finalFont);
 		graphics.setColor(Color.BLACK);
-		graphics.drawString("GAME OVER", 100, 150);
+		graphics.drawString("Game Over", 125, 200);
+		graphics.setFont(finalFont2);
+		graphics.setColor(Color.BLACK);
+		graphics.drawString("You killed 0 enemies", 130, 350);
+		graphics.setFont(titleFont3);
+		graphics.setColor(Color.BLACK);
+		graphics.drawString("Press ENTER to restart", 120, 500);
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (currentState == MENU_STATE) {
-			System.out.println("Menu state");
+
 			updateMenuState();
 		} else if (currentState == GAME_STATE) {
-			System.out.println("Game State");
+
 			updateGameState();
 		} else if (currentState == END_STATE) {
-			System.out.println("Game State");
+
 			updateEndState();
 		}
 		// TODO Auto-generated method stub
@@ -100,49 +124,55 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("Greetings");
 
-		if (e.getKeyCode()==10) {
-		
-			if(currentState== MENU_STATE) {
-				currentState= GAME_STATE;
-				System.out.println("Changing menu state to game state");
-		
+		if (e.getKeyCode() == 10) {
+
+			if (currentState == MENU_STATE) {
+				currentState = GAME_STATE;
+
 			}
-		
-			else if (currentState== GAME_STATE) {
-				currentState= END_STATE;
-				System.out.println("Changing game state to end state");
-		
+
+			else if (currentState == GAME_STATE) {
+				currentState = END_STATE;
+
 			}
-		
-			else if (currentState== END_STATE) {
-				currentState= MENU_STATE;
+
+			else if (currentState == END_STATE) {
+				currentState = MENU_STATE;
 			}
-		
-		
+
 			if (currentState > END_STATE) {
-				currentState= MENU_STATE;
-				System.out.println("Changing from game state to end state");
-		
+				currentState = MENU_STATE;
+
 			}
 		}
 		rocket.update();
-		
-		
+		if (e.getKeyCode() == 37) {
+			rocket.moveLeft();
+		}
+		if (e.getKeyCode() == 38) {
+			rocket.moveDown();
+		}
+		if (e.getKeyCode() == 39) {
+			rocket.moveRight();
+		}
+		if (e.getKeyCode() == 40) {
+			rocket.moveUp();
+		}
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
 		
+		rocket.stop();
 	}
 
 }
--- src/org/jointheleague/level2/leagueInvaders/Rocketship.java
+++ src/org/jointheleague/level2/leagueInvaders/Rocketship.java
@@ -5,17 +5,38 @@ import java.awt.Graphics;
 
 public class Rocketship extends GameObject{
 	int speed;
+	int ySpeed;
 	public Rocketship(int x, int y, int width, int height) {
 		super(x, y, width, height);
 		speed=5;
+		ySpeed=5;
 		// TODO Auto-generated constructor stub
 	}
 	public void update() {
+		x=x+speed;
+		y=y+ySpeed;
 		
 	}
 	public void draw(Graphics graphics) {
 		graphics.setColor(Color.BLUE);
 		graphics.fillRect(x, y, width, height);
 	}
+	public void moveRight(){
+		speed=5;
+	}
+	public void moveLeft(){
+		speed=-5;
+	}
+	public void moveUp(){
+		ySpeed=5;
+	}
+	public void moveDown(){
+		ySpeed=-5;
+	}
+	public void stop(){
+		speed=0;
+		ySpeed=0;
+	}
+	
 	
 }
