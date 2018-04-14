5fbac52b9b44fd2a4cff5f6bfc6a716fab0bcb95
ThirdTime <league@WorthyMini.local>
2018-04-14 12:14:55 -0700

    4-14-18

---
+++ src/org/jointheleague/level2/LeagueBreakout/LeagueBreakout.java
@@ -0,0 +1,10 @@
+package org.jointheleague.level2.LeagueBreakout;
+
+import javax.swing.JFrame;
+
+public class LeagueBreakout {
+	JFrame frame;
+	static final int HEIGHT = 500;
+	static final int WIDTH = 500;
+	GamePanel panel = new GamePanel();
+}
---
+++ src/org/jointheleague/level2/Warm_Up.java
@@ -0,0 +1,15 @@
+package org.jointheleague.level2;
+
+import java.util.ArrayList;
+
+public class Warm_Up {
+	public static void main(String[] args) {
+		ArrayList<String> names = new ArrayList<>();
+		names.add("John");
+		names.add("Isis");
+		names.set(0, "Esme");
+		System.out.println(names);
+	}
+	
+	
+}
