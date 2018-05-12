582a8e7be1c36d245cd4e1214de33390ce4e47d6
ThirdTime <league@WorthyMini.local>
2018-05-12 10:19:02 -0700

    5-12-18
    Worked on game

---
+++ src/org/jointheleague/level2/LeagueBreakout/GamePanel.java
@@ -0,0 +1,7 @@
+package org.jointheleague.level2.LeagueBreakout;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+
+}
--- src/org/jointheleague/level2/LeagueBreakout/LeagueBreakout.java
+++ src/org/jointheleague/level2/LeagueBreakout/LeagueBreakout.java
@@ -1,10 +1,25 @@
 package org.jointheleague.level2.LeagueBreakout;
 
+import java.awt.Component;
+
 import javax.swing.JFrame;
 
 public class LeagueBreakout {
 	JFrame frame;
 	static final int HEIGHT = 500;
 	static final int WIDTH = 500;
-	GamePanel panel = new GamePanel();
+	GamePanel gamePanel = new GamePanel();
+
+	public LeagueBreakout() {
+		frame = new JFrame();
+	}
+
+	public void setUp() {
+		frame.add(gamePanel);
+		frame.setVisible(true);
+	}
+	public static void main(String[] args) {
+		LeagueBreakout game= new LeagueBreakout();
+		game.setUp();
+	}
 }
